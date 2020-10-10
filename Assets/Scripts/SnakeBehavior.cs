a136de3937e3dfc0abe6272bf2752f8f1f9c457c
Jan Inge Nygård <jan.nygard@bouvet.no>
2020-10-10 18:57:54 +0200

    Objects wont collide

--- Assets/Scripts/SnakeBehavior.cs
+++ Assets/Scripts/SnakeBehavior.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class SnakeBehavior : MonoBehaviour
 {
-    public float moveSpeed = 3;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public Vector3 eulerAngles;
     public Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
+        // Will replace the default inside of unity, ie. 3
+        moveSpeed = 10;
+        rotateSpeed = 10;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "strawberry")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            // Debug.Log("Do something else here");
+            moveSpeed = 100;
+        }
     }
 
     // Update is called once per frame
@@ -17,5 +35,29 @@ public class SnakeBehavior : MonoBehaviour
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
         transform.position += new Vector3(move.x, move.y, 0) * moveSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.eulerAngles = Vector3.forward * 0;
+            move.y = 1;
+            move.x = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            move.y = -1;
+            move.x = 0;
+            transform.eulerAngles = Vector3.forward * 180;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            move.x = 1;
+            move.y = 0;
+            transform.eulerAngles = Vector3.forward * -90;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move.x = -1;
+            move.y = 0;
+            transform.eulerAngles = Vector3.forward * 90;
+        }
     }
 }
