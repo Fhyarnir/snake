b4f313eb0ffd4a06272f95c41cab8b449adb7be0
Jan Inge Nygård <jan.nygard@bouvet.no>
2020-10-10 18:02:47 +0200

    Head is now moving

--- Assets/Scripts/SnakeBehavior.cs
+++ Assets/Scripts/SnakeBehavior.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SnakeBehavior : MonoBehaviour
 {
+    public float moveSpeed = 3;
+    public Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(move.x, move.y, 0) * moveSpeed * Time.deltaTime;
     }
 }
